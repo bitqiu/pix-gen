c9f570357a34b2df88ea205fccfda268356fa218
bitqiu <i@bitqiu.cc>
2024-05-13 10:43:47 +0800

    Add cors middleware

--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"github.com/bitqiu/pix-gen/handler"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+	// cors
+	config := cors.DefaultConfig()
+	config.AllowCredentials = true
+	config.AllowOrigins = []string{"*"}
+	r.Use(cors.New(config))
+
 	r.GET("/captcha", handler.HandleCaptcha)
 	r.GET("/qrcode", handler.HandleQrcode)
 	r.Run(":8080")
